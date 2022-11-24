dcacc95325773332a5858f723edcf00e4e8cc24f
Ilya Makarov <mortimer08@gmail.com>
2022-11-24 21:38:48 +0500

    Task58 Complete

---
+++ Task58/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задача 58: Задайте две матрицы.
+// Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+
+// 2 4 | 3 4
+// 3 2 | 3 3
+
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] CreateMatrixRndInt(int rows, int columns, int SumLine, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(SumLine, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],2}");
+            Console.Write(j < matrix.GetLength(1) - 1 ? " " : "");
+        }
+        Console.WriteLine("]");
+    }
+
+}
+
+int[,] MatrixMultiplication(int[,] matrix1, int[,] matrix2)
+{
+    int[,] resultMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix1.GetLength(1); k++)
+            {
+                resultMatrix[i, j] += matrix1[i, k] * matrix2[k, j];
+            }
+        }
+    }
+    return resultMatrix;
+}
+
+bool IsMatrixesMultiplicatuble(int[,] matrix1, int[,] matrix2)
+{
+    return (matrix1.GetLength(1) == matrix2.GetLength(0));
+}
+
+int[,] matrixRnd1 = CreateMatrixRndInt(2, 3, 1, 9);
+int[,] matrixRnd2 = CreateMatrixRndInt(2, 2, 1, 9);
+
+PrintMatrix(matrixRnd1);
+Console.WriteLine();
+PrintMatrix(matrixRnd2);
+
+if (IsMatrixesMultiplicatuble(matrixRnd1, matrixRnd2))
+{
+    Console.WriteLine();
+    int[,] multiplicatedMatrix = MatrixMultiplication(matrixRnd1, matrixRnd2);
+    PrintMatrix(multiplicatedMatrix);
+}
+else
+{
+    System.Console.WriteLine("Матрицы не совместимы");
+}
