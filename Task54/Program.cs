a673c0333b85f03188b96524856e3242ec634448
Ilya Makarov <mortimer08@gmail.com>
2022-11-24 20:28:21 +0500

    Task54 Completed

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 54: Задайте двумерный массив.
+// Напишите программу, которая упорядочит по убыванию
+// элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],2}");
+            Console.Write(j < matrix.GetLength(1) - 1 ? " " : "");
+        }
+        Console.WriteLine("]");
+    }
+
+}
+
+void MatrixLinesSorting(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                int temporary = default;
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    temporary = matrix[i, k];
+                    matrix[i, k] = matrix[i, k + 1];
+                    matrix[i, k + 1] = temporary;
+                }
+            }
+        }
+    }
+}
+
+int[,] matrixRnd = CreateMatrixRndInt(6, 6, 1, 9);
+PrintMatrix(matrixRnd);
+Console.WriteLine();
+MatrixLinesSorting(matrixRnd);
+PrintMatrix(matrixRnd);
